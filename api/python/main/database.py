0152af35310edb6da1444a8e5d9c0bec98de4ffd
ywateba <ywateba@gmail.com>
2026-03-21 20:20:51 +0100

    added docker compose

--- api/python/main/database.py
+++ api/python/main/database.py
@@ -1,14 +1,19 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 # This creates a file named 'chess.db' in the same folder
-SQLALCHEMY_DATABASE_URL = "sqlite:///./chess.db"
+SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./chess.db")
 
 # connect_args={"check_same_thread": False} is needed only for SQLite
-engine = create_engine(
-    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
-)
+if SQLALCHEMY_DATABASE_URL.startswith("sqlite"):
+    engine = create_engine(
+        SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
+    )
+else:
+    engine = create_engine(SQLALCHEMY_DATABASE_URL)
+
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
